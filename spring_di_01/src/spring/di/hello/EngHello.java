8684452baff0dbf01173a1bff0cac4ed2a084796
kgmyh <kgmyh73@gmail.com>
2015-11-18 11:41:56 +0900

    asaaa

--- spring_di_01/src/spring/di/hello/EngHello.java
+++ spring_di_01/src/spring/di/hello/EngHello.java
@@ -5,7 +5,7 @@ public class EngHello implements Hello {
 	public String sayHello(String name){
 		System.out.println("hello");
 		System.out.println("김성환 변경");
-		System.out.println("hi");
+		System.out.println("hiabcdef");
 		System.out.println("권효균 변경");
 		return "Hello! "+name;
 	}
